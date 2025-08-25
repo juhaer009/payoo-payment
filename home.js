cdbd83ea50df28865eed8b10e62c0555d2f4f9f3
juhaer009 <juhaerjafid1998@gmail.com>
2025-08-26 01:44:55 +0600

    added cashout feature

--- home.js
+++ home.js
@@ -1,4 +1,6 @@
 const validPin = 1234;
+const agentPin = 5678;
+// add money feature
 document.getElementById('add-money-btn').
     addEventListener('click', function(){
         const bank = document.getElementById('bank').value;
@@ -21,6 +23,30 @@ document.getElementById('add-money-btn').
         const updatedBalance = addAmmount + availableBalance;
         document.getElementById('available-balance').innerText = updatedBalance;
     });
+
+    // cash out feature
+    document.getElementById('withdraw-btn').
+        addEventListener('click',function(){
+            const agent = document.getElementById('agent').value;
+            const ammount = parseInt(document.getElementById('withdraw-money').value);
+            const pinNumber = parseInt(document.getElementById('agent-pin').value);
+            // console.log(agent,ammount,pinNumber);
+            const availableBalance = parseInt(document.getElementById('available-balance').innerText);
+            if (agent.length < 11){
+                alert('Please provide valid Agent Number');
+                return;
+            }
+
+            if(pinNumber !== agentPin){
+                alert('Please provide valid Pin Number');
+                return;
+            }
+
+            const updatedBalance = availableBalance - ammount;
+            document.getElementById('available-balance').innerText = updatedBalance;
+
+        });
+
 
     // toggling feature
 
