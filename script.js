c95b8c189df0b545007f296d9ab688e545c10318
juhaer009 <juhaerjafid1998@gmail.com>
2025-08-25 22:37:21 +0600

    added home page

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 document.getElementById('login').
     addEventListener('click', function (event){
-        console.log('button clicked')
+        const mobileNumber = 1896217218;
+        const pinNumber = 1234;
+
+        const mobileNumberValue = parseInt(document.getElementById('mobile-number').value);
+        const pinNumberValue = parseInt(document.getElementById('pin-number').value); 
+
+        // console.log(mobileNumberValue, pinNumberValue);
+
+        if (mobileNumber===mobileNumberValue && pinNumber===pinNumberValue){
+            window.location.href = './home.html';
+        }
+        else {
+            alert('Invalid Credential');
+        }
     });
