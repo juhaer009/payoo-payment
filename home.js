2443fb2ea57454d0dd1f2257831de9d8ebc235ff
juhaer009 <juhaerjafid1998@gmail.com>
2025-08-26 23:20:28 +0600

    added reusable functions for add money and cashout feature

--- home.js
+++ home.js
@@ -1,14 +1,52 @@
 const validPin = 1234;
 const agentPin = 5678;
+
+// function for getting value number
+function getValueNumber(id){
+    return parseInt(document.getElementById(id).value);
+}
+// function for getting value
+function getValue(id){
+    return document.getElementById(id).value;
+}
+
+// function to fetch innertext
+function getInnerText(id){
+    return parseInt(document.getElementById(id).innerText);
+}
+// function to set innertext
+function setInnerText(value){
+    document.getElementById('available-balance').innerText = value;
+}
+
+// function for toggling feature
+function handleToggle(id){
+    const forms = document.getElementsByClassName('form');
+        for(const form of forms){
+            form.style.display='none';
+        }
+        document.getElementById(id).style.display='block';
+}
+
+// funtion for focusing on button
+function focusButton(id){
+    const formBtns = document.getElementsByClassName('form-btn');
+            for(const btn of formBtns){
+                btn.classList.remove('border-[#0874f2]', 'bg-[#0874f20d]', 'text-[#0874f2]')
+            }
+            document.getElementById(id).classList.remove('border-gray-300');
+            document.getElementById(id).classList.add('border-[#0874f2]', 'bg-[#0874f20d]', 'text-[#0874f2]');
+}
+
 // add money feature
 document.getElementById('add-money-btn').
     addEventListener('click', function(){
-        const bank = document.getElementById('bank').value;
-        const accountNumber = document.getElementById('account-number').value;
-        const addAmmount = parseInt(document.getElementById('add-ammount').value);
-        const pinNumber = parseInt(document.getElementById('add-pin').value);
+        const bank = getValue('bank');
+        const accountNumber = getValue('account-number');
+        const addAmmount = getValueNumber('add-ammount');
+        const pinNumber = getValueNumber('add-pin');
         // console.log(bank, accountNumber, addAmmount, pinNumber);
-        const availableBalance = parseInt(document.getElementById('available-balance').innerText);
+        const availableBalance = getInnerText('available-balance');
         // console.log(availableBalance);
         if (accountNumber.length < 11){
             alert('Please provide valid Account Number');
@@ -21,17 +59,17 @@ document.getElementById('add-money-btn').
         }
 
         const updatedBalance = addAmmount + availableBalance;
-        document.getElementById('available-balance').innerText = updatedBalance;
+        setInnerText(updatedBalance);
     });
 
     // cash out feature
     document.getElementById('withdraw-btn').
         addEventListener('click',function(){
-            const agent = document.getElementById('agent').value;
-            const ammount = parseInt(document.getElementById('withdraw-money').value);
-            const pinNumber = parseInt(document.getElementById('agent-pin').value);
+            const agent = getValue('agent');
+            const ammount = getValueNumber('withdraw-money');
+            const pinNumber = getValueNumber('agent-pin');
             // console.log(agent,ammount,pinNumber);
-            const availableBalance = parseInt(document.getElementById('available-balance').innerText);
+            const availableBalance = getInnerText('available-balance');
             if (agent.length < 11){
                 alert('Please provide valid Agent Number');
                 return;
@@ -43,7 +81,7 @@ document.getElementById('add-money-btn').
             }
 
             const updatedBalance = availableBalance - ammount;
-            document.getElementById('available-balance').innerText = updatedBalance;
+            setInnerText(updatedBalance);
 
         });
 
@@ -52,13 +90,12 @@ document.getElementById('add-money-btn').
 
     document.getElementById('add-btn').
         addEventListener('click', function(){
-            document.getElementById('cash-out-container').style.display = 'none';
-            document.getElementById('add-money-container').style.display = 'block';
+            handleToggle("add-money-container");
+            focusButton('add-btn');
         });
 
     document.getElementById('cash-out-btn').
         addEventListener('click', function(){
-            document.getElementById('add-money-container').style.display = 'none';
-            document.getElementById('cash-out-container').style.display = 'block';
-            
+            handleToggle('cash-out-container');
+            focusButton('cash-out-btn');
         });
